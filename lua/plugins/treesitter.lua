3ef7e84aa85fa2ba14fbb01bcc5141d3412c316c
Francois Qian <francois.qian2@gmail.com>
2024-08-14 17:38:11 +0100

    treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,26 +1,13 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    event = { "BufReadPre", "BufNewFile" },
-    build = function()
-        require("nvim-treesitter.install").update({ with_sync = true })()
-    end,
-    opts = function(_, opts)
-      opts.highlight = { enable = true, },
-      opts.autopairs = { enable = true },
-      opts.textobjects = {
-        move = {
-          enable = true,
-          goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer" },
-          goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer" },
-          goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer" },
-          goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer" },
-        },
-      }
-
-      opts.indent = {enable = true}
-      opts.context_commentstring = {
-        enable = true,
-        enable_autocmd = false,
-      }
-    end,
+	"nvim-treesitter/nvim-treesitter",
+	event = { "BufReadPre", "BufNewFile" },
+	build = function()
+		require("nvim-treesitter.install").update({ with_sync = true })()
+	end,
+	opts = {
+		ensure_installed = {"c", "cpp", "rust", "go", "zig", "asm" },
+	},
+	config = function(_, opts)
+		require("nvim-treesitter.configs").setup(opts)
+	end
 }
