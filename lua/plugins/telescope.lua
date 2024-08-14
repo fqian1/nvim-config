6ac35c1d1c11077af5d5a50da019f2be2e01cc7b
Francois Qian <francois.qian2@gmail.com>
2024-08-14 23:34:30 +0100

    added fzy and frecency and filebrowser to telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,18 +5,24 @@ return {
 		'nvim-lua/plenary.nvim',
 		{ 'nvim-telescope/telescope-ui-select.nvim' },
 		{ 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+		'nvim-telescope/telescope-fzy-native.nvim',
+		'nvim-telescope/telescope-frecency.nvim',
+		'nvim-telescope/telescope-file-browser.nvim',
 	},
 	opts = {
 		defaults = {},
 		extensions = {
 			['ui-select'] = {
 				require('telescope.themes').get_dropdown(),
-			}
-		}
+			},
+		},
 	},
 
 	config = function(_, opts)
 		require('telescope').setup(opts)
 		require('telescope').load_extension('ui-select')
+		require('telescope').load_extension('fzy_native')			-- Will be used automatically
+		require('telescope').load_extension('frecency')				-- Will be used automatically
+		require('telescope').load_extension('file_browser')
 	end
 }
